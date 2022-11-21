6697ea0d4c5115b6fe0e3def4b47582a7080ff6a
mlsmith55 <mlsmith55@gmail.com>
2022-11-21 11:31:04 +0100

    Update CancerGrowth_LogModel.py

--- Cancer Cell Growth/Logarithmic Model/CancerGrowth_LogModel.py
+++ Cancer Cell Growth/Logarithmic Model/CancerGrowth_LogModel.py
@@ -6,7 +6,7 @@ Created on Wed Jun 15 17:41:24 2022
 @author: Mike Best to save this code on your own computer/file including the links for the two generated figues.
 """
 print()
-print("The semi-log model for dilute cancer cell growth.")
+print("The semi-log model for dilute cancer cell replication in culture media.")
 
 # import files
 import numpy as np
@@ -25,17 +25,20 @@ ydata = exampledata[:,1]
 error = exampledata[:,3]
 
 #define the function - 
-#the model to be examined, where x represents the independent variable values and V (volts) and RC (resistivity) are the parameters to be estimated. This is the linear model with two parameters.
+#The model to be examined, where x represents the independent variable, time, B is the initial cell count and C is a measure of replication per hour. 
+#This is the linear model with two parameters.
 def func(x,B,C):
     return B + (x*C)
 
 #The initial guesses of the model parameters
-p0=[3.0,0.01] #The first value is the log(voltage) guess, the second value for the guess for resistivity.
+p0=[3.0,0.01] #The first value is the guess of the log(initial count), the second value for the guess of replication.
 
-#Nonlinear fit of the model to the data. The bnds are the two minima and then the two maxima search ranges for V and RC parameters. The sigma uses the error data from the selected column of the .csv file.
+#Nonlinear fit of the model to the data. The bnds are the two minima and then the two maxima search ranges for B and C parameters. 
+#The sigma uses the error data from the selected column of the .csv file.
 bnds = ([1.0,0.001],[500.0,1.0])
 params, pcov = curve_fit(func,xdata,ydata, p0, bounds = bnds, sigma = error, absolute_sigma = False)
-#Above - the error are the values from line 25, absolute_sigma = False means that the standard deviation is not selected by the program but we let the errors be the file values. 
+#Above - the error are the values from line 25, absolute_sigma = False means that the standard deviation is not selected by the program 
+#but we let the errors be the file values. 
 
 #Extracting the two parameters and S.D. from the curve_fit.
 ans_b, ans_c = params #ans_b is log(V) and ans_c is RC
@@ -52,18 +55,15 @@ rans_c_SD = round(ans_c_SD,5)
 b=ans_b
 c=ans_c
 
-#solve, again, the function to be used for the nonlinear plot
+#solve the function to be used for the nonlinear plot
 funcdata = func(xdata,b,c)
 
-#Estimating the goodness of fit from the difference between the observed distance data (ydata) and the calculated distances using the term on the right-hand side, below.
+#Estimating the goodness of fit from the difference between the observed distance data (ydata) and the calculated distances 
+#using the term on the right-hand side, below.
 chisq = sum((ydata - func(xdata,ans_b,ans_c))**2/(error**2))
 chisquar = round(chisq,4)
 #normalised chisquar is calculated with the number of observations (60) and 2 the number of parameters 
 normchisquar = round((chisquar/(60-2)),5)
-#The BIC value is calculated as
-BIC = 60 * np.log10(chisq/60) + 2*np.log10(60)
-normBIC = round(BIC,2)
-#BIC is the acronym for Bayesian Information Criteria
 
 #To calculate the r**2 value
 resids = ydata - func(xdata,ans_b,ans_c)
@@ -85,7 +85,7 @@ ax.tick_params(axis="x", direction='in', length=10)
 for axis in ['top','bottom','left','right']:
     ax.spines[axis].set_linewidth(3)
     ax.tick_params(width=3)
-plt.xlabel("Time (Hours)", fontsize=18)
+plt.xlabel("Time (hours)", fontsize=18)
 plt.ylabel("$log_{e}(cells)$", fontsize=18)
 plt.rc('xtick', labelsize=14) 
 plt.rc('ytick', labelsize=14)
@@ -104,8 +104,6 @@ print("The r\u00b2 value is calculated as: ",r2)
 print("The goodness of fit, \u03C7\u00b2, is: ", chisquar)
 print("The reduced goodness of fit, \u03C7\u00b2, is: ", normchisquar)
 print("Reduced \u03C7\u00b2 = \u03C7\u00b2/(N-P), where N are the number of data pairs and P is the parameter count.")
-print("The calculated BIC is: ", normBIC)
-print("BIC represents the Bayesian Information Criteria")
 
 #Routines to save figues in eps and pdf formats
 fig.savefig("LogCancerGrowth_vs_time.eps", format="eps", dpi=2000, bbox_inches="tight", transparent=True)
