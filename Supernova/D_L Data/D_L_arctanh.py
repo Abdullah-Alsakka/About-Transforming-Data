6d81ecd8eeff8fd0d0f1526fc850217607362ad8
mlsmith55 <mlsmith55@gmail.com>
2022-11-25 07:51:17 +0100

    Update D_L_arctanh.py

--- Supernova/D_L Data/D_L_arctanh.py
+++ Supernova/D_L Data/D_L_arctanh.py
@@ -14,10 +14,11 @@ import csv
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-#open data file selecting the distance, distance standard deviation and recession velocity columns
+# open data file selecting the distance, distance standard deviation and recession velocity columns
 with open("Gold_Riess_D_L_2004.csv","r") as i:
     rawdata = list(csv.reader(i, delimiter = ","))
     
+# ignoring the first row
 exampledata = np.array(rawdata[1:],dtype=float)
 
 xdata = exampledata[:,3]
@@ -30,57 +31,57 @@ def func(x,b,c):
     return (litesped/(x*b*np.sqrt(abs(1-c))))*np.sinh(2*(np.arctanh(np.sqrt(abs(1-c)))-np.arctanh(np.sqrt(abs(1-c))/np.sqrt((c/x)+ (1-c)))))
 
 #The initial guesses of the model parameters
-p0=[69.0,0.05]
+p0=[70.0,0.05]
 
-#define the lightspeed constant
+# define the lightspeed constant
 litesped = 299793
 
-#define the parameters
-b=64.8
-c=0.0001
+# define the parameters
+b=ans_b
+c=ans_c
 
-#evaluate and plot function
+# evaluate and plot function
 funcdata = func(xdata,b,c)
 
-#curve fit model to the data, where the first pair are the minimum bounds and the second pair the maximum bounds 
+# curve fit model to the data, where the first pair are the lower bounds and the second pair the upper bounds 
 bnds = ([50.0, 0.0001],[80.0,1.0])
+
+# when absolute_sigma = False the regression routine "normalizes" the standard deviations (errors)
 params, pcov = curve_fit(func,xdata,ydata, p0, bounds = bnds, sigma = error, absolute_sigma = False)
 
-#extracting the Hubble constant, normalized matter density and rounding the values
+# extracting the Hubble constant, normalized matter density and rounding the values
 ans_b, ans_c = params
 rans_b = round(ans_b,2)
 rans_c = round(ans_c,3)
 
-#extracting the S.D. of both above values
+# extracting the S.D. of both above values
 perr = np.sqrt(np.diag(pcov))
 ans_b_SD, ans_c_SD = perr
 rans_b_SD = round(ans_b_SD,2)
 rans_c_SD = round(ans_c_SD,3)
 
-#Estimating the goodness of fit
+# estimating the goodness of fit
 chisq = sum((ydata[1:-1] - func(xdata,ans_b,ans_c)[1:-1])**2/func(xdata,ans_b,ans_c)[1:-1])
 chisquar = round(chisq,2)
 #normalised chisquar is calculated as 
 normchisquar = round((chisquar/(158-2)),2)
 
-#calculation of residuals
+# calculation of residuals
 residuals = ydata - func(xdata,ans_b,ans_c)
 ss_res = np.sum(residuals**2)
 ss_tot = np.sum((ydata-np.mean(ydata))**2)
 
-#r squared calculation
+# r squared calculation
 r_squared = 1 - (ss_res/ss_tot)
 r2 = round(r_squared,3)
 r2adjusted = round(1-(((1-r2)*(len(ydata)-1))/(len(ydata)-len(params)-1)),3)
 
-#plot of imported data
+# plot of imported data
 plt.figure(1,dpi=240)
-
-#plt.plot(xdata,ydata,label = "Observed SNe Ia data")
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams['lines.linewidth'] = 3
 plt.xlim(0.5,1.0)
-plt.ylim(0.0,15000)
+plt.ylim(0.0,18000)
 plt.xscale("linear")
 plt.yscale("linear")
 fig, ax = plt.subplots()
@@ -97,18 +98,16 @@ plt.plot(xdata, funcdata, color = "orange", label = "Arctanh Model")
 plt.legend(loc='best', fancybox=True, shadow=False)
 plt.show()
 
-#print results
+# print results
 print()
 print("The calculated Hubble constant with standard deviation are: ", rans_b,",", rans_b_SD )
 print("The calculated normalized matter density with standard deviation are: ", rans_c,",", rans_c_SD )
 print("The adjusted r\u00b2 is calculated to be: ",r2adjusted)
-print("The r\u00b2 is calculated to be: ",r2)
-print("The goodness of fit \u03C7\u00b2 is: ", chisquar)
+#print("The r\u00b2 is calculated to be: ",r2)
+#print("The goodness of fit \u03C7\u00b2 is: ", chisquar)
 print("The reduced goodness of fit \u03C7\u00b2 is: ", normchisquar)
-print("Reduced \u03C7\u00b2 = \u03C7\u00b2/(N-P), where N are the number of data pairs and P is the parameter count.")
-print(f"D_L is {func(1,rans_b,rans_c)} when expansion factor is 1")
 
-#Routines to save figues in eps and pdf formats
+# Routines to save figues in eps and pdf formats
 fig.savefig("Arctanh_D_L_data.eps", format="eps", dpi=1200, bbox_inches="tight", transparent=True)
 fig.savefig("Arctanh_D_L_data.pdf", format="pdf", dpi=1200, bbox_inches="tight", transparent=True)
 
