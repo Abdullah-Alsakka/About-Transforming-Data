762e333d89d3c82813c99c81cf322e775cf84b94
mlsmith55 <mlsmith55@gmail.com>
2022-11-26 10:03:46 +0100

    Update mag_LCDM.py

--- Supernova/mag Data/mag_LCDM.py
+++ Supernova/mag Data/mag_LCDM.py
@@ -6,7 +6,7 @@ Created on Fri Jul 29 17:53:20 2022
 @author: mike
 """
 print("A test of the Friedmann-Lemaitre-Robertson-Walker (FLRW) model using the curve_fit regression routine of Python scipy uses the SNe Ia data, as mag vs redshift (z), from the Gold data set of Riess, A.G. et al. 'Type Ia Supernova Discoveries at z> 1 from the Hubble Space Telescope: Evidence for Past Deceleration and Constraints on Dark Energy Evolution' Astrophys. J. vol. 607(2), 665-687 (2004). This variation of the LCDM model used here has two parameters: Hubble constant, Hu, normalised matter density, O_m; the cosmological constant is the remainder of information in a universe with flat geometry.")
-print("This is the magLCDM model, typically knwon as the standard model of coslmology.")
+print("This is the magLCDM model, typically known as the standard model of cosmology.")
 
 # import the data file and the Python 3 libraries
 import numpy as np
@@ -15,9 +15,10 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scipy import integrate as intg
 
-#open data file
+# open data file
 with open("Gold_Riess_mag_2004.csv","r") as i:
     rawdata = list(csv.reader(i, delimiter = ","))
+    
 # ignore the top row    
 exampledata = np.array(rawdata[1:],dtype=float)
 
@@ -25,7 +26,7 @@ xdata = exampledata[:,1]
 ydata = exampledata[:,2]
 error = exampledata[:,3]
 
-# initial guess for the normailized matter density, O_m
+# initial guess for the normalized matter density, O_m
 O_m = 0.30
 
 # where t is the "dummy" variable during integration
@@ -35,38 +36,41 @@ def integr(x,O_m):
 def func2(x, O_m):
     return np.asarray([integr(xx,O_m) for xx in x]) 
 
+# specify the speed of light
 litesped = 299793
 
 def func3(x,Hu,O_m):
     return 5*(np.log10((litesped*(1+x)/Hu)*np.sinh(func2(x,O_m)))) + 25
 
-# guesses for the Hubble constant, Hu, and the normlaized matter density, O_m
+# guesses for the Hubble constant, Hu, and the normalized matter density, O_m
 init_guess = np.array([70,0.30])
 
 # allowed range for the two parameters
 bnds=([50,0.01],[80,1.0])
 
+# fitting the model to the data, note that when choosing absolute_sigma = False the standard deviations (error) are normalized
 params, pcov = curve_fit(func3, xdata, ydata,p0 = init_guess, bounds = bnds, sigma = error, absolute_sigma = False)
 
-#Extracting and rounding the parameter values
+# extracting and rounding the parameter, Hu and O_m, values
 ans_Hu, ans_O_m = params
 rans_Hu = round(ans_Hu,2)
 rans_O_m = round(ans_O_m,3)
 
-#Extracting and rounding the estimated oarameter standard deviations.
+# extracting and rounding the estimated standard deviations.
 perr = np.sqrt(np.diag(pcov))
 SD_Hu, SD_O_m = perr
 rSD_Hu = round(SD_Hu,2)
 rSD_O_m = round(SD_O_m,3)
 
-#estimating the goodness of fit
+# estimating the goodness of fit
 chisq = sum((ydata - func3(xdata,ans_Hu,ans_O_m))**2/func3(xdata,ans_Hu,ans_O_m))
 chisquar = round(chisq,4)
-#normalised chisquar is calculated for 157 data pairs with P the parameter count as
+
+# normalised chisquar is calculated for 157 data pairs with P the parameter count (2) as
 P=2
 normchisquar = round((chisquar/(157-P)),4)
 
-#calculation of residuals,again
+# calculation of residuals
 residuals = ydata - func3(xdata,ans_Hu,ans_O_m)
 ss_res = np.sum(residuals**2)
 ss_tot = np.sum((ydata-np.mean(ydata))**2)
@@ -102,9 +106,9 @@ print()
 print("The calculated Hubble constant and S.D. are: ", rans_Hu, ",",rSD_Hu)
 print("The calculated normalised matter density and S.D. are: ",rans_O_m, ",",rSD_O_m )
 print("The adjusted r\u00b2 is calculated to be: ",r2adjusted)
-print("The goodness of fit \u03C7\u00b2 is: ", chisquar)
+#print("The goodness of fit \u03C7\u00b2 is: ", chisquar)
 print("The reduced goodness of fit \u03C7\u00b2 is: ", normchisquar)
-print("Reduced \u03C7\u00b2 = \u03C7\u00b2/(N-P), where N are the number of data pairs and P is the parameter count.")
+#print("Reduced \u03C7\u00b2 = \u03C7\u00b2/(N-P), where N are the number of data pairs and P is the parameter count.")
 
 #commands to save plots in two different formats
 fig.savefig("flatLCDM_mag_data.eps", format="eps", dpi=2000, bbox_inches="tight", transparent=True)
