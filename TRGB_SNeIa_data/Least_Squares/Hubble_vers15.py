13b0d0e811e97a8c42826885e8f717645ea00bcb
mlsmith55 <mlsmith55@gmail.com>
2025-08-15 11:17:10 +0200

    Update Hubble_vers15.py
    minor edits.

--- TRGB_SNeIa_data/Least_Squares/Hubble_vers15.py
+++ TRGB_SNeIa_data/Least_Squares/Hubble_vers15.py
@@ -6,7 +6,11 @@ This refers to Eqs. 7 of the manuscript.
 @author: Mike
 """
 print()
-print("This curve_fit regression routine of Python scipy, uses the 'distance mag' data, converted to luminosity distances, D_L, vs redshift. Data from Brout et al. 2022, 'The Pantheon+ Analysis: Cosmological Constraints' Astrophys. J. vol. 938, 110 and W. Freedman et al. 2019 The Carnegie-Chicago Hubble Program. VIII. ApJ vol. 882, 34. Here we use the Hubble model with only a single parameter, the Hubble constant; no values are calculated for matter density, dark energy or space geometry. The Python 3 least_squares robust regression routine is employed with the specification that the routine specifies loss='cauchy' or 'linear'.")
+print("This curve_fit regression routine of Python scipy, uses the 'distance mag' data, converted to luminosity distances, D_L, vs redshift.") 
+print("Data from Brout et al. 2022, 'The Pantheon+ Analysis: Cosmological Constraints' Astrophys. J. vol. 938, 110 and W. Freedman et al. 2019") 
+print("The Carnegie-Chicago Hubble Program. VIII. ApJ vol. 882, 34. Here we use the Hubble model with only a single parameter, the Hubble constant;") 
+print("no values are calculated for matter density, dark energy or space geometry. The Python 3 least_squares robust regression routine is employed")
+print("with the specification that the routine specifies loss='cauchy' or 'linear' (w/o robust routine).")
 
 import csv
 import numpy as np
@@ -41,7 +45,7 @@ def func1(params, x, y):
 result2 = least_squares(func1, x0=params, jac='3-point', bounds=(50,90), method = 'trf', loss='linear', args=(x, ydata))
 Hu, = result2.x
 
-#yfit1 now describes the Rh model
+#yfit1 now describes the R_h model
 yfit1 = litesped*x/Hu
 Hubble = np.round(Hu,2)
 
@@ -55,7 +59,7 @@ r_squared_lsq = 1 - (ss_res_lsq/ss_tot_lsq)
 #r2 = round(r_squared,3)
 r2_lsq = round(r_squared_lsq,3)
 
-# where P is the parameter count, N the number of data pairs (1720), the r2adjusted value rounded to 4 digits
+# where P is the parameter count, N the number of data pairs (1720), the r^2 the adjusted value rounded to 4 digits
 P=1
 N=1720
 e = 2.718281
@@ -69,14 +73,16 @@ cov = np.linalg.inv(J.T.dot(J))
 perr = np.sqrt(np.diagonal(cov))
 StndDev, = perr
 
-#We write a comma after StndDev, because we are extracting this value from tuple. Below is just rounding off the value of the standard deviation to something one might believe to only 3 digits.
+#We write a comma after StndDev, because we are extracting this value from tuple. Below is just rounding off the value of the standard deviation 
+#to something one might believe to only 3 digits.
 normSD = round(StndDev,3)
 
-#calculate the statistical fitness, using 1702 as the number of data pairs and 1 as the degree of freedom (parameter count). Note the error values are of the SNe Ia distances.
+#calculate the statistical fitness, using 1720 as the number of data pairs and 1 as the degree of freedom (parameter count). 
+#Note the error values are of the SNe Ia distances.
 chisq = sum((ydata-yfit1)**2/(errors)**2)
 normchisquar = np.round((chisq/(N-P)),2)
 
-#calculate the statistical fitness, in the more usual manner, with 1702 as the number of data pairs and 3 as the degree of freedom (paramater count)
+#calculate the statistical fitness, in the more usual manner, with 1720 as the number of data pairs and 1 as the degree of freedom (paramater count)
 chisqed = sum(((ydata[1:-1]-yfit1[1:-1])**2)/yfit1[1:-1])
 normchisquared = np.round((chisqed/(N-P)),2)
 
